c1c0de1c0f6ae87c3f70d2507b2d42f4e0a7e7dc
Kelvin Filyk <kelvinfilyk@Kelvins-MacBook-Pro-2.local>
2021-05-11 13:59:30 -0700

    Changed port to 3000 for express

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express()
-const port = 4000;
+const port = 3000; /* default express port */
 
 app.use(express.static(__dirname+'/src'))
 app.get('/', (req, res) => res.sendFile('index.html'))
