1d22180bed10a01c8bb2fd8df3b4c28af7fcb3d4
Kelvin Filyk <kelvinfilyk@Kelvins-MacBook-Pro-2.local>
2021-05-11 14:05:22 -0700

    Changed port to env. specified dynamic port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express()
-const port = 3000; /* default express port */
+const PORT = process.env.PORT || 5000;; /* default express port */
 
 app.use(express.static(__dirname+'/src'))
 app.get('/', (req, res) => res.sendFile('index.html'))
-app.listen(port, () => console.log('App listening on port ' + port))
+app.listen(PORT, () => console.log('App listening on port ' + PORT))
